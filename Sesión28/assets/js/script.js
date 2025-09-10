f353137965ef0be93b4aa0dbaee7bde918d7dd86
Asher Chozo <dasespinozachozo@crackthecode.la>
2025-09-09 19:28:55 -0500

    Estructura Sesión 36

--- Sesión28/assets/js/script.js
+++ Sesión28/assets/js/script.js
@@ -1,23 +1,26 @@
 /*
-Función -> Es un bloque de código reutilizable que puede tomar valores para transformarlos. Además también puede retornar algún valor.
+Función -> Es un bloque de código reutilizable que puede tomar 
+valores para transformarlos. Además también puede retornar 
+algún valor.
 
-Sintaxis básica:
+Sintaxis:
 
 function nombreFuncion() {
   // Código de mi función
-  // Todo este codigo se ejecutará cada vez que yo "llame" a mi función
+  // Todo este codigo se ejecutará cada vez
+  // que yo "llame" a mi función
 }
 
-Dato importante: Lo mas recomendable para nombrar funciones, es hacerlo con verbos descriptivos en infinitivo
+Dato importante: Lo mas recomendable para nombrar funciones,
+es hacerlo con verbos descriptivos en infinitivo.
 */
 
 console.log("FUNCIONES");
-// Ejemplo básico: Escribe un saludo, muestra un numero y muestra tu nombre;
-
+// Ejemplo: Escribe un saludo, muestra un número y muestra tu nombre;
 // Definir una función
 function mostrarMensajes() {
   // Codigo de la funcion
-  console.log("Inicio")
+  console.log("Inicio");
   console.log("Buenas noches");
   console.log(8);
   console.log("Valeria");
@@ -36,9 +39,9 @@ function mostrarMensajes() {
 // console.log("LLAMADA #5");
 // mostrarMensajes();
 
-// Reto 1: Crea una function que imprima los numeros del 1 al 10 (1 por 1)
+// Reto 1: Crea una function que imprima los numeros
+// del 1 al 10 (1 por 1)
 console.log("Reto 1");
-
 // Definimos la funcion
 function showNumbers() {
   console.log(1);
@@ -56,23 +59,25 @@ function showNumbers() {
 // Llamamos a la funcion
 // showNumbers();
 // showNumbers();
+/* Parametros -> Son valores que son pasados a
+la funcion para que esta los pueda usar, transformar, 
+modificar, operar, etc.
 
-/*
-Parametros -> Son valores que son pasados a la funcion para que esta los pueda usar, transformar, modificar, operar, etc.
-
-Los parametros van dentro de los parentesis, en este caso serian a y b. El nombre que le das a los parametros es el que usaras dentro de la funcion.
+Los parametros van dentro de los parentesis, en este 
+caso serian a y b. El nombre que le das a los 
+parametros es el que usaras dentro de la funcion.
 
 function nombreFuncion(a, b) {
   // Código de mi función
   // Dentro de mi funcion podria hacer cosas con a y b
 }
-
 Los parametros son posicionales
 */
 
-// Ejemplo
-// Crear una funcion que toma dos numeros y muestre su suma
-// Scope -> Alcance, hace referencia a un bloque de codigo donde una variable puede ser usada.
+// Crear una funcion que toma dos numeros y
+// muestre su suma
+// Scope -> Alcance, hace referencia a un bloque
+// de codigo donde una variable puede ser usada.
 
 function addTwoNumbers(x, y) {
   console.log(`Primer parametro: ${x}`);
@@ -89,14 +94,19 @@ function addTwoNumbers(x, y) {
 let num1 = 14;
 let num2 = 25;
 
-// Cuando pasas los valores de los parametros, estos se llaman argumentos
+// Cuando pasas los valores de los parametros,
+// estos se llaman argumentos
 // addTwoNumbers(num1, num2);
 
-console.log("RETORNO DE VALORES")
-// Retorno de valores -> Es la caracteristica que tiene una funcion de retornar un valor al finalizar su ejecucion. Ese valor puede ser almacenado en una variable o usado directamente.
+console.log("RETORNO DE VALORES");
+// Retorno de valores -> Es la caracteristica que
+// tiene una funcion de retornar un valor al finalizar
+// su ejecucion. Ese valor puede ser almacenado en una
+// variable o usado directamente.
 
 // Ejemplo
-// Crear una funcion que reciba como parametro un nombre y muestre un mensaje de saludo
+// Crear una funcion que reciba como parametro
+// un nombre y muestre un mensaje de saludo
 
 function greet(targetName) {
   let message = `Hola ${targetName}, espero tengas un buen día.`;
@@ -104,8 +114,9 @@ function greet(targetName) {
   return message;
 }
 
-let name = 'Valeria';
-// Llamada a la funcion, al retornar un valor, podemos almacenarlo
+let name = "Valeria";
+// Llamada a la funcion, al retornar un
+// valor, podemos almacenarlo
 let greetMessage = greet(name);
 // Y luego usamos ese dato almacenado
 console.log(greetMessage);
@@ -113,8 +124,11 @@ console.log(greetMessage);
 // O tambien podemos usar el valor de retorno directamente
 console.log(greet(name));
 
-console.log("ARROW FUNCTIONS")
-// Arrow functions (Funciones de flecha) -> Son una sintaxis distinta para escribir funciones, fueron introducidas en ECMAScript6. Se llaman asi por el simbolo de =>
+console.log("ARROW FUNCTIONS");
+// Arrow functions (Funciones de flecha) -> Son una
+// sintaxis distinta para escribir funciones, fueron
+// introducidas en ECMAScript6. Se llaman asi
+// por el simbolo de =>
 // Funcion anonima
 
 let value1 = 14;
@@ -124,7 +138,7 @@ let multiplyTwoNumbers = (a, b) => {
   console.log(`Factor 1 -> ${a}`);
   console.log(`Factor 2 -> ${b}`);
 
-  let result = a*b;
+  let result = a * b;
   let message = `${a} x ${b} = ${result}`;
 
   return message;
